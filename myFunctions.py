af2169236c6c88a4e40baa7c86daf6b6225281c6
Geoffrey Noakes <geof.noakes@gmail.com>
2020-03-30 10:40:17 -0400

    First Commit of functions

---
+++ myFunctions.py
@@ -0,0 +1,14 @@
+
+
+def square ( x ):
+  return (x*x)
+
+
+def cube( x ):
+  return (x*x*x)
+
+def printManyTimes (s, x):
+  for i in range(x):
+    print(s)
+
+    
---
+++ myTester.py
@@ -0,0 +1,8 @@
+
+from myFunctions import square, cube, printManyTimes
+
+print(square(3))
+
+print(cube(3))
+
+printManyTimes("Hello", 5)
