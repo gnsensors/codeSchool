247c8d6e7747b5a7aa01353a44d4c0550bd196a9
Geoffrey Noakes <geof.noakes@gmail.com>
2020-04-20 13:37:39 -0400

    added word search

---
+++ wordList.py
@@ -0,0 +1,41 @@
+from itertools import permutations
+
+
+f = open('wordlist.txt', 'r')
+
+tmp = f.readlines()
+
+fullWordList=[]
+for i in tmp:
+  i.replace('\n', '')
+  fullWordList.append(i.replace('\n', ''))
+
+#print(fullWordList)
+  
+letterList = ['e', 'i', 'u', 'g', 'n', 'n', 'p']
+
+tmpComboList = list(permutations(letterList,  5))
+
+comboList = []
+
+for w in tmpComboList:
+  x = ''
+  comboList.append(x.join(w))
+
+#print(comboList)
+
+words = []
+for x in comboList:
+  if (x in fullWordList):
+    words.append(x)
+    
+mylist = list(dict.fromkeys(words))
+print(mylist)
+
+# print("there are " + str(len(words))+ " combinations of letters")
+# print(words)
+
+# counter = 0
+# while counter<len(words):
+#   print(words[counter])
+#   counter=counter+1
